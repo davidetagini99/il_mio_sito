590adb7191d601abbf85ef8460d9806a9671eac4
davidetagini99 <davide.tagini@yahoo.com>
2024-02-22 11:59:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <main></main>
+</body>
+</html>
